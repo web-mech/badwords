9a64d720581e57e8177ced6933acd8314f745ccc
Andrew Wright <awright@tenthousandcoffees.com>
2018-10-18 11:17:08 -0400

    feat(removeWords): allow array of words to remove
    When adding words, you can add strings or an array of strings, this change
    allows you to add an array of words or a list of words to exclude from
    the profane filter.

--- test/removeWords.js
+++ test/removeWords.js
@@ -4,12 +4,17 @@ var Filter = require('../lib/badwords.js'),
   assert = require('better-assert');
 
 describe('filter', function(){
-  describe('removeWords',function(){
-    it("Should allow you to remove words from the filter blacklist and no longer filter them",function(){
+  describe('removeWords', function(){
+    it("Should allow you to remove words from the filter blacklist and no longer filter them", function(){
       filter.removeWords('hells');
       assert(filter.clean('This is a hells good test') === 'This is a hells good test');
       filter.addWords('hells');
       assert(filter.clean('This is a hells good test') === 'This is a ***** good test');
+    });
+
+    it("Should allow you to add an array of bad words to filter", function(){
+      filter.removeWords(['hells','crap']);
+      assert(filter.clean('This is a hells a crap test') === 'This is a hells a crap test');
     });
   });
 });
