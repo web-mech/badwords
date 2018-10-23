aec750fbdc9b59eb1f9ddfdbc80fb1d162d3dcf2
webmech <webmech@gmail.com>
2018-10-23 14:42:19 -0400

    test(addWords): update tests to reflect usage

--- test/addWords.js
+++ test/addWords.js
@@ -6,8 +6,15 @@ var Filter = require('../lib/badwords.js'),
 describe('filter', function(){
   describe('addWords',function(){
     it('Should append words to the filter list.', function(){
-      filter.addWords('dog');
-      assert(filter.clean('Go dog go') === 'Go *** go');
+      filter.addWords('dog', 'go');
+      assert(filter.clean('Go dog go') === '** *** **');
+    });
+
+    it('Should append words to the filter using an array', () => {
+      let addWords = ['go', 'dog'];
+      filter = new Filter()
+      filter.addWords(...addWords);
+      assert(filter.clean('Go dog go') === '** *** **');
     });
 
     it('Should allow a list to be passed to the constructor', function() {
